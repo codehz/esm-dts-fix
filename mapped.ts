6c27889306e4c40826ec886c52750c3fcfd5259e
CodeHz <codehz@outlook.com>
2021-07-17 13:50:52 +0800

    add fix for esm.sh

---
+++ mapped.ts
@@ -0,0 +1,9 @@
+// deno-fmt-ignore-file
+
+export default [
+  ["/v43/react@17.0.2/index.d.ts", "/v43/@types/react@17.0.11/index.d.ts"],
+  ["/v41/react@17.0.2/deno/index.d.ts.js", "/v43/@types/react@17.0.11/index.d.ts"],
+  ["/v43/@types/react@17.0.2/index.d.ts", "/v43/@types/react@17.0.11/index.d.ts"],
+  ["cdn.esm.sh", "^^HOST^^/_"],
+  ["esm.sh", "^^HOST^^"],
+] as Array<[string, string]>;
--- mod.ts
+++ mod.ts
@@ -1,10 +1,41 @@
 /// <reference path="./deploy.d.ts" />
 
+import mapped from "./mapped.ts";
+
 addEventListener("fetch", async (event) => {
   const url = new URL(event.request.url);
   const dts = url.searchParams.get("dts");
   if (!dts) {
-    event.respondWith(new Response(null, { status: 403 }));
+    const origin = url.pathname.startsWith("/_/")
+      ? new URL(url.pathname.substr(11), "https://cdn.esm.sh/")
+      : new URL(url.pathname, "https://esm.sh/");
+    origin.search = url.search;
+    const res = await fetch(origin, {
+      headers: {
+        "User-Agent": "Deno/fix-dts",
+      },
+    });
+    if (res.status != 200) {
+      event.respondWith(res);
+      return;
+    }
+    let dts = res.headers.get("x-typescript-types") ?? undefined;
+    let text = await res.text();
+    for (const [key, value] of mapped) {
+      text = text.replaceAll(key, value);
+      dts = dts?.replace(key, value);
+    }
+    text = text.replaceAll("^^HOST^^", url.host);
+    dts = dts?.replace("^^HOST^^", url.host);
+    event.respondWith(
+      new Response(text, {
+        status: 200,
+        headers: {
+          ...Object.fromEntries(res.headers.entries()),
+          "x-typescript-types": dts as string,
+        },
+      }),
+    );
   } else {
     url.searchParams.delete("dts");
     const origin = new URL(url.pathname, "https://esm.sh/");
