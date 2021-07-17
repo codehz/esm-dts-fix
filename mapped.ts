7a7ae76ec24273eba9bae166e322a33a2734773f
CodeHz <codehz@outlook.com>
2021-07-17 14:20:44 +0800

    match versions

--- mapped.ts
+++ mapped.ts
@@ -1,9 +1,9 @@
 // deno-fmt-ignore-file
 
 export default [
-  ["/v43/react@17.0.2/index.d.ts", "/v43/@types/react@17.0.11/index.d.ts"],
-  ["/v41/react@17.0.2/deno/index.d.ts.js", "/v43/@types/react@17.0.11/index.d.ts"],
-  ["/v43/@types/react@17.0.2/index.d.ts", "/v43/@types/react@17.0.11/index.d.ts"],
+  [/\/v43\/react@17\.0\.\d+\/index\.d\.ts\//g, "/v43/@types/react@17.0.14/index.d.ts"],
+  [/\/v41\/react@17\.0\.\d+\/deno\/index\.d\.ts\.js/g, "/v43/@types/react@17.0.14/index.d.ts"],
+  [/\/v43\/@types\/react@17\.0\.\d+\/index\.d\.ts/g, "/v43/@types/react@17.0.14/index.d.ts"],
   ["cdn.esm.sh", "^^HOST^^/_"],
   ["esm.sh", "^^HOST^^"],
-] as Array<[string, string]>;
+] as Array<[string | RegExp, string]>;
